48335b577db389083805936c8f1a2a98d1995299
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2021-12-29 02:48:05 -0500

    Install Contentlayer.

---
+++ contentlayer.config.ts
@@ -0,0 +1,17 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import highlight from 'rehype-highlight'
+
+export const Post = defineDocumentType(() => ({
+	name: 'Post',
+	filePathPattern: `**/*.md`,
+	fields: {
+		title: { type: 'string', required: true },
+		date: { type: 'date' },
+	},
+}))
+
+export default makeSource({
+	contentDirPath: 'posts',
+	documentTypes: [Post],
+	markdown: { rehypePlugins: [highlight] },
+})
