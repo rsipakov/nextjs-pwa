fe315d2d18402bf8b2b1cf96f95eb9cf38f5bfbc
Willin Wang <willin@willin.org>
2021-12-30 17:49:58 +0800

    feat: :bug: fix i18n

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,17 +1,17 @@
-import { useMDXComponent } from 'next-contentlayer/hooks';
-import { getTweets } from '@/lib/twitter';
-import components from 'components/MDXComponents';
-import BlogLayout from '@/layouts/blog';
-import Tweet from '@/components/Tweet';
-import { allBlogs } from '.contentlayer/data';
-import type { Blog } from '.contentlayer/types';
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import { getTweets } from '@/lib/twitter'
+import components from 'components/MDXComponents'
+import BlogLayout from '@/layouts/blog'
+import Tweet from '@/components/Tweet'
+import { allBlogs } from '.contentlayer/data'
+import type { Blog } from '.contentlayer/types'
 
 export default function Post({ post, tweets }: { post: Blog; tweets: any[] }) {
-	const Component = useMDXComponent(post.body.code);
+	const Component = useMDXComponent(post.body.code)
 	const StaticTweet = ({ id }) => {
-		const tweet = tweets.find((tweet) => tweet.id === id);
-		return <Tweet {...tweet} />;
-	};
+		const tweet = tweets.find((tweet) => tweet.id === id)
+		return <Tweet {...tweet} />
+	}
 
 	return (
 		<BlogLayout post={post}>
@@ -19,24 +19,29 @@ export default function Post({ post, tweets }: { post: Blog; tweets: any[] }) {
 				components={
 					{
 						...components,
-						StaticTweet
+						StaticTweet,
 					} as any
 				}
 			/>
 		</BlogLayout>
-	);
+	)
 }
 
 export async function getStaticPaths() {
 	return {
-		paths: allBlogs.map((p) => ({ params: { slug: p.slug } })),
-		fallback: false
-	};
+		paths: allBlogs.map((p) => ({
+			params: { slug: p.slug },
+			locale: p.locale,
+		})),
+		fallback: false,
+	}
 }
 
-export async function getStaticProps({ params }) {
-	const post = allBlogs.find((post) => post.slug === params.slug);
-	const tweets = await getTweets(post.tweetIds);
+export async function getStaticProps({ params, locale }) {
+	const post = allBlogs.find(
+		(post) => post.slug === params.slug && post.locale === locale
+	)
+	const tweets = await getTweets(post.tweetIds)
 
-	return { props: { post, tweets } };
+	return { props: { post, tweets } }
 }
