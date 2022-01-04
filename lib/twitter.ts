3449d9486c2179b39091505a206cef1f2cd85708
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-04 00:01:08 -0500

    Change to "TWITTER_BEARER_TOKEN"

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -20,7 +20,7 @@ export const getTweets = async (ids) => {
 		`https://api.twitter.com/2/tweets?${queryParams}`,
 		{
 			headers: {
-				Authorization: `Bearer ${process.env.TWITTER_API_KEY}`
+				Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` // Twitter Bearer Token. Also, add domain 'pbs.twimg.com' to next.config.js
 			}
 		}
 	);
