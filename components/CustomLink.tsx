5cefc67d1ac7ced3e62f8fb04ed8d090a425b806
Rostyslav Sipakov <sipakov.rv@missioniz.com>
2022-01-09 18:44:58 -0500

    chore: add CustomLink and Image components

--- components/CustomLink.tsx
+++ components/CustomLink.tsx
@@ -1,23 +1,27 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import Link from 'next/link'
+import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
 
-const CustomLink = ({ href, ...rest }) => {
-  const isInternalLink = href && href.startsWith('/')
-  const isAnchorLink = href && href.startsWith('#')
+const CustomLink = ({
+											href,
+											...rest
+										}: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
+	const isInternalLink = href && href.startsWith('/')
+	const isAnchorLink = href && href.startsWith('#')
 
-  if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    )
-  }
+	if (isInternalLink) {
+		return (
+			<Link href={href}>
+				<a {...rest} />
+			</Link>
+		)
+	}
 
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />
-  }
+	if (isAnchorLink) {
+		return <a href={href} {...rest} />
+	}
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+	return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
 }
 
 export default CustomLink
--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from '@/components/Image'
 import { parseISO, format } from 'date-fns'
 import ViewCounter from '@/components/ViewCounter'
 import type { PropsWithChildren } from 'react'
